313c67103c05d9d5f6c7322946cfc05efacf4b52
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-01-10 04:37:30 -0500

    func added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -25,6 +25,6 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: "C", withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+               //
     }
 }
